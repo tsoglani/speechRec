ceea93b0189c55bdfbd1b34c1182a037537af232
tsoglani <mrtsoglanakos@hotmail.com>
2016-09-28 22:40:18 +0300

    Initial commit

---
+++ Jarvis/src/jarvis/Jarvis.java
@@ -0,0 +1,428 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jarvis;
+
+import com.darkprograms.speech.microphone.Microphone;
+import com.darkprograms.speech.recognizer.GSpeechDuplex;
+import com.darkprograms.speech.recognizer.GSpeechResponseListener;
+import com.darkprograms.speech.recognizer.GoogleResponse;
+import jaco.mp3.player.MP3Player;
+import java.io.File;
+//import com.sun.speech.freetts.Voice;
+//import com.sun.speech.freetts.VoiceManager;
+import java.io.IOException;
+import java.io.InputStream;
+import static java.lang.Thread.sleep;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.LineUnavailableException;
+import net.sourceforge.javaflacencoder.FLACFileWriter;
+import weather.Weather;
+
+/**
+ *
+ * @author tsoglani
+ */
+public class Jarvis {
+
+    /**
+     * @param args the command line arguments
+     */
+    private Microphone mic;
+    private GSpeechDuplex duplex;
+    public static final String name = "";
+    private static Weather weather;
+    private String output;
+    private String bigOutput = "";
+
+    public static void main(String[] args) {
+        new Jarvis();
+
+    }
+
+    public Jarvis() {
+        dospeak("hello sir");
+        playAudio();
+        weather = new Weather();
+        activate();
+
+    }
+    private Thread thread;
+    private int counter;
+    private int sleepTime = 10;
+    private final int totalWaitTime = 2000;
+
+    public void activate() {
+        mic = new Microphone(FLACFileWriter.FLAC);
+
+        duplex = new GSpeechDuplex("AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw");
+
+        duplex.setLanguage("en");
+
+        try {
+            mic.open();
+            duplex.recognize(mic.getTargetDataLine(), mic.getAudioFormat());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (LineUnavailableException ex) {
+            ex.printStackTrace();
+        }
+
+        duplex.addResponseListener(new GSpeechResponseListener() {
+//            String old_text = "";
+
+            public void onResponse(GoogleResponse gr) {
+
+                System.out.println(gr.getResponse());
+                if (gr.getResponse() == null || gr.getResponse().replaceAll(" ", "").equalsIgnoreCase("")) {
+                    processRespond(bigOutput);
+                    System.out.println("output=" + output + "  ::prevOut=" + bigOutput);
+
+                    bigOutput = "";
+                    output = "";
+                    counter = 0;
+
+                } else {
+                    output = gr.getResponse();
+                }
+                if (bigOutput == null || bigOutput.length() <= output.length()) {
+                    bigOutput = output;
+//                    System.out.println(output);
+                }
+
+                if (thread == null) {
+                    thread = new Thread() {
+                        @Override
+                        public void run() {
+                            counter = totalWaitTime;
+
+                            try {
+                                while (counter > 0) {
+                                    sleep(sleepTime);
+                                    counter -= sleepTime;
+                                }
+                                if (output != null || !output.replaceAll(" ", "").equals("")) {
+                                    output = "";
+                                    if (bigOutput != null && !bigOutput.replaceAll(" ", "").equals("")) {
+                                        processRespond(bigOutput);
+                                        bigOutput = "";
+                                        deActivate();
+                                        activate();
+                                    }
+                                }
+                            } catch (InterruptedException ex) {
+                                Logger.getLogger(Jarvis.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                            thread = null;
+                        }
+                    };
+                    thread.start();
+                } else {
+                    counter = totalWaitTime;
+
+                }
+
+//                if (gr != null && gr.getResponse() != null && !output.replaceAll(" ", "").equals("")) {
+//                    if (thread == null) {
+//                        thread = new Thread() {
+//                            @Override
+//                            public void run() {
+//                                try {
+//                                    sleep(1000);
+//                                    processRespond(output);
+//
+//                                } catch (InterruptedException ex) {
+//                                    Logger.getLogger(Jarvis.class.getName()).log(Level.SEVERE, null, ex);
+//                                }
+//                                thread = null;
+//                            }
+//                        };
+//                        thread.start();
+//                    }
+////                    processRespond(output);
+//                }
+            }
+        });
+    }
+
+    String respondText = "";
+
+    private void processRespond(String respond) {
+
+        System.out.println("respond::" + respond);
+        if (respondText.replaceAll(" ", "").contains(name + "hi") || respondText.replaceAll(" ", "").contains(name + "hello")
+                || respondText.replaceAll(" ", "").contains("hi" + name) || respondText.replaceAll(" ", "").contains("hello" + name)) {
+            dospeak("wellcome sir");
+            respondText = "";
+        } else if (respond.replaceAll(" ", "").contains(name + "aresokind") || respond.replaceAll(" ", "").contains("aresokind" + name)) {
+            dospeak("Thank you sir.");
+            respondText = "";
+        } else if (respond.replaceAll(" ", "").contains(name + "hi") || respond.replaceAll(" ", "").contains(name + "hello")
+                || respond.replaceAll(" ", "").contains("hi" + name) || respond.replaceAll(" ", "").contains("hello" + name)) {
+            dospeak("wellcome sir");
+            respondText = "";
+        } else if (respond.replaceAll(" ", "").contains(name + "stop")||respond.replaceAll(" ", "").contains(name + "cancel")) {
+            stopSpeak();
+            respondText = "";
+        } else if (respond.replaceAll(" ", "").contains("weatherin" + name) || respond.replaceAll(" ", "").contains(name + "weatherin")) {
+            String[] list = respond.split("weather in");
+            try {
+                if (list.length > 1) {
+                    String curentCity = list[1];
+                    if (curentCity.length() > 1) {
+                        if (curentCity.contains(" of ")) {
+                            String[] surentList = curentCity.split(" of ");
+                            curentCity = surentList[0];
+                            String countryCode = surentList[1];
+                            String weatherInfo = weather.getWeather(curentCity.substring(1, curentCity.length()), countryCode);
+                            dospeak(weatherInfo);
+                        } else if (curentCity != null && !curentCity.equals("")) {
+                            String weatherInfo = weather.getWeather(curentCity.substring(1, curentCity.length()));
+                            dospeak(weatherInfo);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+//            dospeak("wellcome sir");
+
+//            String weatherInfo = weather.getWeather(curentCity);
+//            dospeak(weatherInfo);
+            respondText = "";
+
+        } else if (respond.replaceAll(" ", "").contains("weather" + name) || respond.replaceAll(" ", "").contains(name + "weather")) {
+
+            String weatherInfo;
+            if (respond.replaceAll(" ", "").contains("forecast")) {
+                weatherInfo = weather.getForecastWeatherAtCity("Ierapetra");
+
+            } else {
+                weatherInfo = weather.getWeather("Ierapetra");
+            }
+            dospeak(weatherInfo);
+            respondText = "";
+
+        } else if (respond.replaceAll(" ", "").contains("whatis") && respond.replaceAll(" ", "").contains(name)
+                || respond.replaceAll(" ", "").contains(name + "whatis")
+                || respond.replaceAll(" ", "").contains("what's") && respond.replaceAll(" ", "").contains(name)
+                || respond.replaceAll(" ", "").contains(name + "what's")
+                || respond.replaceAll(" ", "").contains("search") && respond.replaceAll(" ", "").contains(name)) {
+
+            String spStr = null;
+            try {
+
+                if (respond.replaceAll(" ", "").contains("whatis")) {
+                    spStr = getFromWiki(respond.split("what is")[1]);
+                } else if (respond.replaceAll(" ", "").contains("what's")) {
+                    spStr = getFromWiki(respond.split("what's")[1]);
+                } else if (respond.replaceAll(" ", "").contains("search")) {
+                    spStr = getFromWiki(respond.split("search")[1]);
+                }
+                if (spStr != null) {
+//                    System.out.println("getFromWiki:" + spStr);
+                    dospeak(spStr);
+
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(Jarvis.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            respondText = "";
+        } else if (respond.replaceAll(" ", "").contains("playaudio" + name)
+                || respond.replaceAll(" ", "").contains(name + "playaudio")
+                || respond.replaceAll(" ", "").contains("playsound" + name)
+                || respond.replaceAll(" ", "").contains(name + "playsound")
+                || respond.replaceAll(" ", "").contains("playmusic" + name)
+                || respond.replaceAll(" ", "").contains(name + "playmusic")
+                || respond.replaceAll(" ", "").contains("playsong" + name)
+                || respond.replaceAll(" ", "").contains(name + "playsong")) {
+
+            dospeak("right away sir.");
+            playAudio();
+            respondText = "";
+
+        } else if (respond.replaceAll(" ", "").contains("nextsong")
+                || respond.replaceAll(" ", "").contains("next")) {
+            nextSong();
+
+        } else if (respond.replaceAll(" ", "").contains("previous")
+                || respond.replaceAll(" ", "").contains("previoussong")) {
+            previousSong();
+            respondText = "";
+
+        } else if (respond.replaceAll(" ", "").contains("howareyou" + name) || respond.replaceAll(" ", "").contains(name + "howareyou")) {
+            dospeak("I am fine sir, thanks for asking. Would you like something else.");
+        } else if (!respondText.contains(respond)) {
+            respondText += " " + respond;
+        }
+    }
+
+    private String getFromWiki(String searchingText) throws MalformedURLException, IOException {
+//https://en.wikipedia.org/w/api.php?action=opensearch&search=java-programming&limit=1&format=json
+        String[] list = searchingText.split(" ");
+        String searchingAddingString = "";
+        for (int i = 0; i < list.length; i++) {
+            searchingAddingString += list[i];
+            if (i < list.length - 1) {
+                searchingAddingString += "-";
+            }
+        }
+        if (searchingAddingString.equals("")) {
+            return null;
+        }
+
+        String urlString = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchingAddingString + "&limit=1&format=json";
+        URL url = new URL(urlString);
+        URLConnection con = url.openConnection();
+        InputStream in = con.getInputStream();
+        String encoding = con.getContentEncoding();
+        encoding = encoding == null ? "UTF-8" : encoding;
+        String body = org.apache.commons.io.IOUtils.toString(in, encoding);
+        System.out.println(body);
+//        body.replaceAll("\\[", "@@@");
+//        body.replaceAll("\\]", "@@@");
+
+        String[] listBody = body.split("\\[\"");
+        try {
+            String output = listBody[3].substring(0, listBody[3].length() - 3);
+            System.out.println(output);
+            return output;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+//    private static final String VOICENAME_kevin = "kevin";
+//    Voice voice;
+//    VoiceManager voiceManager;
+    private void stopSpeak() {
+        if (child != null) {
+            child.destroyForcibly();
+        }
+
+        if (mp3Player != null) {
+            mp3Player.stop();
+        }
+
+    }
+    Process child;
+////sudo apt-get install espeak
+
+    public void dospeak(String speaktext) {
+        try {
+            //        if (speaktext == null || speaktext.replaceAll(" ", "").equals("")) {
+//            return;
+//        }
+//
+//        if (voice != null) {
+//            voice.deallocate();
+//        }
+//        if (voiceManager == null) {
+//            voiceManager = VoiceManager.getInstance();
+//        }
+//        voice = voiceManager.getVoice(VOICENAME_kevin);
+//        voice.allocate();
+//
+//        voice.speak(speaktext);
+
+            if (child != null) {
+                child.destroyForcibly();
+            }
+
+////sudo apt-get install espeak
+            Runtime.getRuntime().exec(new String[]{"espeak", '\"' + speaktext + '\"'});
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void deActivate() {
+        if (mic != null && mic.getTargetDataLine().isOpen()) {
+
+            mic.close();
+        }
+        if (duplex != null) {
+            try {
+                duplex.addResponseListener(null);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            duplex = null;
+        }
+        System.gc();
+//        if (voice != null) {
+//            voice.deallocate();
+//        }
+//        voice = null;
+
+    }
+    private MP3Player mp3Player;
+    private String musicFilesLocation = "/media/tsoglani/tsoglani1/Music";
+    File musicFile = new File(musicFilesLocation);
+    int musicCounterID = ((int) (Math.random() * (musicFile.list().length - 1)));
+    private boolean isGoingToPlayNext = false;
+    Thread playerThread;
+
+    private void playAudio() {
+
+        try {
+            if (musicCounterID >= musicFile.list().length) {
+                musicCounterID = 0;
+            }
+//            File cf = new File(musicFilesLocation + "/" + musicFile.list()[musicCounterID++]);
+//            if (cf.isDirectory() || !cf.exists()) {
+//                playAudio();
+//            }
+//            String bip = cf.getAbsolutePath();
+//            System.out.println(bip);
+////            AudioInputStream audioIn = AudioSystem.getAudioInputStream(cf);
+////            Clip clip = AudioSystem.getClip();
+////            clip.open(audioIn);
+////            clip.start();
+            if (mp3Player != null && !mp3Player.isStopped()) {
+                mp3Player.stop();
+            }
+
+            mp3Player = new MP3Player();
+
+            String[] files = musicFile.list();
+            for (int i = 0; i < files.length; i++) {
+                String title = files[i];
+                File cf = new File(musicFilesLocation + "/" + title);
+                if (cf.isFile()) {
+                    mp3Player.addToPlayList(cf);
+                }
+
+            }
+            mp3Player.play();
+
+        } catch (Exception ex) {
+            Logger.getLogger(Jarvis.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private void previousSong() {
+        if (mp3Player != null) {
+            mp3Player.skipBackward();
+        }
+
+    }
+
+    private void nextSong() {
+        if (mp3Player != null) {
+            mp3Player.skipForward();
+        }
+
+    }
+
+}
---
+++ Jarvis/src/weather/Weather.java
@@ -0,0 +1,283 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weather;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bitpipeline.lib.owm.OwmClient;
+import org.bitpipeline.lib.owm.WeatherData;
+import org.bitpipeline.lib.owm.WeatherStatusResponse;
+import org.json.JSONException;
+import com.maxmind.geoip.Location;
+import com.maxmind.geoip.LookupService;
+import com.maxmind.geoip.regionName;
+import java.io.File;
+import org.bitpipeline.lib.owm.WeatherForecastResponse;
+
+public class Weather {
+
+    public Weather() {
+
+    }
+
+    private String city, countryCode;
+
+    public String getWeather() {
+        setCurentCityAndCode();
+        return getWeather(city, countryCode);
+    }
+
+    public String getWeather(String city) {
+        setCurentCityAndCode();
+        this.city = city;
+        System.out.println("city0"+city);
+        return getWeather(city, countryCode);
+
+    }
+
+    public String getForecastWeatherAtCity() {
+        setCurentCityAndCode();
+        this.city = city;
+        return getWeather(city, countryCode);
+    }
+
+    public String getForecastWeatherAtCity(String city) {
+        this.city = city;
+        this.countryCode = countryCode;
+
+        String description = null;
+
+        try {
+            OwmClient owm = new OwmClient();
+            WeatherForecastResponse forcast = owm.forecastWeatherAtCity(city);
+            if (forcast.hasForecasts()) {
+
+                WeatherData weather = forcast.getForecasts().get(0);
+
+                if (weather.getPrecipitation() == Integer.MIN_VALUE) {
+                    WeatherData.WeatherCondition weatherCondition = weather.getWeatherConditions().get(0);
+
+                    description = weatherCondition.getDescription();
+                    description += ", wind will be " + weather.getWind().getSpeed() + " km per hour";
+
+                    description += ", Temperature will be around " + Math.round(weather.getTemp() - 273) + " degrees Celsius";
+                    description += ", Humidity will be at " + weather.getHumidity() + " per cent";
+
+                    if (description.contains("rain") || description.contains("shower")) {
+                        System.out.println("No rain measures in " + city + " but reports of " + description);
+                    } else {
+                        System.out.println("No rain measures in " + city + ": " + description);
+                    }
+                } else {
+                    System.out.println("It's raining in " + city + ": " + weather.getPrecipitation() + " mm/h");
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Weather.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+        return description;
+
+    }
+
+    public String getWeather(String city, String countryCode) {
+        this.city = city;
+        this.countryCode = countryCode;
+
+        String description = null;
+String output="";
+        try {
+            OwmClient owm = new OwmClient();
+            WeatherStatusResponse currentWeather = owm.currentWeatherAtCity(city, countryCode);
+            if (currentWeather.hasWeatherStatus()) {
+
+                WeatherData weather = currentWeather.getWeatherStatus().get(0);
+
+                if (weather.getPrecipitation() == Integer.MIN_VALUE) {
+                    WeatherData.WeatherCondition weatherCondition = weather.getWeatherConditions().get(0);
+
+                    description = weatherCondition.getDescription();
+//                    description += ", wind is " + weather.getWind().getSpeed() + " km per hour";
+//
+//                    description += ", Temperature is around " + Math.round(weather.getTemp() - 273) + " degrees Celsius";
+//                    description += ", Humidity is at " + weather.getHumidity() + " per cent";
+
+                    if (description.contains("rain") || description.contains("shower")) {
+                        output+="No rain measures in Tokyo but reports of " + description;
+                       
+                    } else {
+                        output+="No rain measures in " + city + ": " + description;
+                    }
+                } else {
+                   output+="It's raining in " + city + ": " + weather.getPrecipitation() + " mm/h";
+                }
+                
+                output+= ", Temperature is around " + Math.round(weather.getTemp() - 273) + " Celsium";
+                output+= ", Humidity is around " + weather.getHumidity() + " per cent";
+                
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Weather.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+        return output;
+
+    }
+
+    private void setCurentCityAndCode() {
+
+        try {
+            URL whatismyip = new URL("http://checkip.amazonaws.com");
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    whatismyip.openStream()));
+
+            String ip = in.readLine(); //you get the IP as a String
+            System.out.println(ip);
+            GetLocationExample obj = new GetLocationExample();
+            ServerLocation location = obj.getLocation(ip);
+            System.out.println("location.getCity()" + location.getCity());
+            System.out.println("location.getCountryCode()" + location.getCountryCode());
+
+            this.city = location.getCity();
+            this.countryCode = location.getCountryCode();
+        } catch (Exception ex) {
+            Logger.getLogger(Weather.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public class GetLocationExample {
+
+        public ServerLocation getLocation(String ipAddress) {
+
+            File file = new File(
+                    "/home/tsoglani/NetBeansProjects/Jarvis/resources/location/GeoLiteCity.dat");
+            return getLocation(ipAddress, file);
+
+        }
+
+        public ServerLocation getLocation(String ipAddress, File file) {
+
+            ServerLocation serverLocation = null;
+
+            try {
+
+                serverLocation = new ServerLocation();
+
+                LookupService lookup = new LookupService(file, LookupService.GEOIP_MEMORY_CACHE);
+                Location locationServices = lookup.getLocation(ipAddress);
+
+                serverLocation.setCountryCode(locationServices.countryCode);
+                serverLocation.setCountryName(locationServices.countryName);
+                serverLocation.setRegion(locationServices.region);
+                serverLocation.setRegionName(regionName.regionNameByCode(
+                        locationServices.countryCode, locationServices.region));
+                serverLocation.setCity(locationServices.city);
+                serverLocation.setPostalCode(locationServices.postalCode);
+                serverLocation.setLatitude(String.valueOf(locationServices.latitude));
+                serverLocation.setLongitude(String.valueOf(locationServices.longitude));
+
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+
+            return serverLocation;
+
+        }
+
+    }
+
+    public class ServerLocation {
+
+        private String countryCode;
+        private String countryName;
+        private String region;
+        private String regionName;
+        private String city;
+        private String postalCode;
+        private String latitude;
+        private String longitude;
+
+        @Override
+        public String toString() {
+            return city + " " + postalCode + ", " + regionName + " (" + region
+                    + "), " + countryName + " (" + countryCode + ") " + latitude
+                    + "," + longitude;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getCountryCode() {
+            return countryCode;
+        }
+
+        public void setCountryCode(String countryCode) {
+            this.countryCode = countryCode;
+        }
+
+        public String getCountryName() {
+            return countryName;
+        }
+
+        public void setCountryName(String countryName) {
+            this.countryName = countryName;
+        }
+
+        public String getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(String latitude) {
+            this.latitude = latitude;
+        }
+
+        public String getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(String longitude) {
+            this.longitude = longitude;
+        }
+
+        public String getPostalCode() {
+            return postalCode;
+        }
+
+        public void setPostalCode(String postalCode) {
+            this.postalCode = postalCode;
+        }
+
+        public String getRegion() {
+            return region;
+        }
+
+        public void setRegion(String region) {
+            this.region = region;
+        }
+
+        public String getRegionName() {
+            return regionName;
+        }
+
+        public void setRegionName(String regionName) {
+            this.regionName = regionName;
+        }
+    }
+
+}
